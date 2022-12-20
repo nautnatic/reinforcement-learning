8b46e3f1a48933764bd570b2852e89457e3ca9e3
Michael Lauter <michael.lauter1@gmail.com>
2022-12-20 20:41:18 +0100

    added docu to env-sample.sh

--- env-sample.sh
+++ env-sample.sh
@@ -1,3 +1,4 @@
-# Starcraft
+# path to the Starcraft 2 installation
 export SC2PATH=/opt/StarCraftII
+# library path for GPU setup with conda
 export LD_LIBRARY_PATH=/home/michael/miniconda3/envs/tf/lib/
